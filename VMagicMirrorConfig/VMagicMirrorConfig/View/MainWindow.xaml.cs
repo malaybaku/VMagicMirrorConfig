ca0ea3b0ed8ce89da79f6accd44c5811fcdc1af2
malaybaku <njslyr.trpg+harriet@gmail.com>
2019-03-18 21:12:10 +0900

    change tab to dragablz / refactor

--- VMagicMirrorConfig/VMagicMirrorConfig/View/MainWindow.xaml.cs
+++ VMagicMirrorConfig/VMagicMirrorConfig/View/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Diagnostics;
 using System.Windows;
 
@@ -26,49 +25,6 @@ namespace Baku.VMagicMirrorConfig
 
         private void OnShowLicenseClick(object sender, RoutedEventArgs e)
             => new LicenseWindow().ShowDialog();
-
-        #region TabControlっぽく動かすためのイベントハンドラ
-
-        private void OnClickHomeButton(object sender, RoutedEventArgs e)
-        {
-            HideAllTabElements();
-            homeTab.Visibility = Visibility.Visible;
-        }
-
-        private void OnClickBGSettingButton(object sender, RoutedEventArgs e)
-        {
-            HideAllTabElements();
-            backgroundTab.Visibility = Visibility.Visible;
-        }
-
-        private void OnClickLayoutButton(object sender, RoutedEventArgs e)
-        {
-            HideAllTabElements();
-            layoutTab.Visibility = Visibility.Visible;
-        }
-
-        private void OnClickLightButton(object sender, RoutedEventArgs e)
-        {
-            HideAllTabElements();
-            lightTab.Visibility = Visibility.Visible;
-        }
-
-        private void OnClickStartupButton(object sender, RoutedEventArgs e)
-        {
-            HideAllTabElements();
-            startupTab.Visibility = Visibility.Visible;
-        }
-
-        private void HideAllTabElements()
-        {
-            homeTab.Visibility = Visibility.Collapsed;
-            backgroundTab.Visibility = Visibility.Collapsed;
-            layoutTab.Visibility = Visibility.Collapsed;
-            lightTab.Visibility = Visibility.Collapsed;
-            startupTab.Visibility = Visibility.Collapsed;
-        }
-
-        #endregion
 
     }
 }
