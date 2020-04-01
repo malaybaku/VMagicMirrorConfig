a371b95c8b474f0c3721f03fea5a2acaeeb90434
malaybaku <njslyr.trpg+harriet@gmail.com>
2020-04-02 02:47:38 +0900

    add virtual cam related UI

---
+++ VMagicMirrorConfig/VMagicMirrorConfig/View/Windows/CameraInstallWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Baku.VMagicMirrorConfig
+{
+    public partial class CameraInstallWindow : Window
+    {
+        public CameraInstallWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
