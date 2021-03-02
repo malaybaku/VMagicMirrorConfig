e6427eaff902bf99641d9e080ef435d4a193d9db
malaybaku <njslyr.trpg+harriet@gmail.com>
2021-03-02 22:52:03 +0900

    binding fix

--- VMagicMirrorConfig/VMagicMirrorConfig/View/Code/Converter/EmptyToNoneTextConverter.cs
+++ VMagicMirrorConfig/VMagicMirrorConfig/View/Code/Converter/EmptyToNoneTextConverter.cs
@@ -4,16 +4,22 @@ using System.Windows.Data;
 
 namespace Baku.VMagicMirrorConfig
 {
-    public class EmptyToNoneTextConverter : IValueConverter
+    public class EmptyToNoneTextConverter : IMultiValueConverter
     {
-        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
-            => value switch
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length != 2 || !(values[0] is string mainValue) || !(values[1] is string))
             {
-                string s when string.IsNullOrEmpty(s) => LocalizedString.GetString("CommonUi_None"),
-                string s => s,
-                _ => Binding.DoNothing,
-            };
+                return Binding.DoNothing;
+            }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)  =>  Binding.DoNothing;
+            //NOTE: values[1]には言語名が入ってる想定だが、Localized.GetStringで間接的に使うので直接参照しないでよい
+            return string.IsNullOrEmpty(mainValue)
+                ? LocalizedString.GetString("CommonUi_None")
+                : mainValue;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            => new object[] { Binding.DoNothing };
     }
 }
