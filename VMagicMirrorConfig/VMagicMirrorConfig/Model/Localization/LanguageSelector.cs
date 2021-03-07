d0fcf4d88a92f8043efc26a0cea263483701bd8f
malaybaku <njslyr.trpg+harriet@gmail.com>
2021-03-07 23:57:07 +0900

    fallback to English if localize key is missing

--- VMagicMirrorConfig/VMagicMirrorConfig/Model/Localization/LanguageSelector.cs
+++ VMagicMirrorConfig/VMagicMirrorConfig/Model/Localization/LanguageSelector.cs
@@ -54,23 +54,7 @@ namespace Baku.VMagicMirrorConfig
         {
             _sender = sender;
             _dictionaryLoader.LoadFromDefaultLocation();
-        }
-
-        public void InitializePreferredLanguage(string preferredLanguageName)
-        {
-            if (preferredLanguageName == "Japanese" || 
-                preferredLanguageName == "English")
-            {
-                LanguageName = preferredLanguageName;
-            }
-            else
-            {
-                //NOTE: 普通はここには来ない
-                LanguageName =
-                    (CultureInfo.CurrentCulture.Name == "ja-JP") ?
-                    "Japanese" :
-                    "English";
-            }
+            ApplyFallbackLocalize();
         }
 
         private bool IsValidLanguageName(string languageName)
@@ -97,6 +81,37 @@ namespace Baku.VMagicMirrorConfig
 
             Application.Current.Resources.MergedDictionaries[0] = dict;
             _sender?.SendMessage(MessageFactory.Instance.Language(languageName));
+        }
+
+        /// <summary>
+        /// 日英以外のローカライズファイルでキーが漏れている場合に英語で補填する処理
+        /// </summary>
+        private void ApplyFallbackLocalize()
+        {
+            var englishDict = new ResourceDictionary()
+            {
+                Source = new Uri($"/VMagicMirrorConfig;component/Resources/English.xaml", UriKind.Relative),
+            };
+            var englishDictKeys = englishDict.Keys.Cast<string>().ToHashSet();
+
+            foreach (var pair in _dictionaryLoader.GetLoadedDictionaries())
+            {
+                bool missingKeyFound = false;
+                var d = pair.Value;
+                var targetKeys = d.Keys.Cast<string>().ToHashSet();
+                //NOTE: このExceptが重い気がするが、リソースディクショナリの仕様上こうするしかなさそうなので頑張らないでおく
+                foreach (var fallbackKey in englishDictKeys.Except(targetKeys))
+                {
+                    d[fallbackKey] = englishDict[fallbackKey];
+
+                    //全部指摘してもくどいので、各言語についてローカライズ漏れは最大1箇所だけの指摘に留める
+                    if (!missingKeyFound)
+                    {
+                        missingKeyFound = true;
+                        LogOutput.Instance.Write($"Localization '{pair.Key}' does not have key '{fallbackKey}'.");
+                    }
+                }
+            }
         }
     }
 }
