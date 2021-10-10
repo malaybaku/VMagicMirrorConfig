7f20dd9e0264d3d73af397c1dac92d8db7016ada
malaybaku <njslyr.trpg+harriet@gmail.com>
2021-10-10 22:14:51 +0900

    Fix error

--- VMagicMirrorConfig/VMagicMirrorConfig/Common/AppLabels.cs
+++ VMagicMirrorConfig/VMagicMirrorConfig/Common/AppLabels.cs
@@ -8,6 +8,7 @@ namespace Baku.VMagicMirrorConfig
         public static string AppName => "VMagicMirror v1.8.2";
         public static string EditionName => FeatureLocker.FeatureLocked ? "Standard Edition" : "Full Edition";
         public static string AppFullName => AppName + " " + EditionName;
-        public static string AppFullNameWithEnvSuffix => AppFullName + (TargetEnvironmentChecker.CheckIsDebugEnv() ? "(Dev)" : "";
+        public static string AppFullNameWithEnvSuffix => 
+            AppFullName + (TargetEnvironmentChecker.CheckIsDebugEnv() ? "(Dev)" : "");
     }
 }
