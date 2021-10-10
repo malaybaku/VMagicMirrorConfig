7af87bf86e93881992a2c3762d5927d99c63987f
malaybaku <njslyr.trpg+harriet@gmail.com>
2021-10-10 22:07:16 +0900

    Add "(Dev)" suffix to window when it is dev build

--- VMagicMirrorConfig/VMagicMirrorConfig/Common/AppLabels.cs
+++ VMagicMirrorConfig/VMagicMirrorConfig/Common/AppLabels.cs
@@ -1,4 +1,6 @@
-﻿namespace Baku.VMagicMirrorConfig
+﻿using Baku.VMagicMirrorConfig.Model;
+
+namespace Baku.VMagicMirrorConfig
 {
     //NOTE: このクラスで、メインウィンドウやライセンスに表示する名称を管理します。
     public static class AppLabels
@@ -6,5 +8,6 @@
         public static string AppName => "VMagicMirror v1.8.2";
         public static string EditionName => FeatureLocker.FeatureLocked ? "Standard Edition" : "Full Edition";
         public static string AppFullName => AppName + " " + EditionName;
+        public static string AppFullNameWithEnvSuffix => AppFullName + (TargetEnvironmentChecker.CheckIsDebugEnv() ? "(Dev)" : "";
     }
 }
---
+++ VMagicMirrorConfig/VMagicMirrorConfig/Model/LocalUtil/TargetEnvironmentChecker.cs
@@ -0,0 +1,15 @@
+﻿namespace Baku.VMagicMirrorConfig.Model
+{
+    public static class TargetEnvironmentChecker
+    {
+        public static bool CheckIsDebugEnv()
+        {
+#if DEV_ENV
+            //DEV_ENV フラグは、dev系のpublish profileでビルドすると定義される
+            return true;
+#endif
+            //Unityからパイプ情報が渡されてない = Unity側がエディタ実行であると考えられる時、デバッグ実行と判断できる
+            return !CommandLineArgParser.TryLoadMmfFileName(out _);
+        }
+    }
+}
